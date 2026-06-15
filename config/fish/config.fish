43d2c6b671d33787ff30d37a5b117143d5a1669a
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-06-15 18:56:34 +0530

    feat: optimize fish init with tool caching

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,11 +11,31 @@ if status is-interactive
         end
     end
 
-    # Initialize tools with Fish integration
-    init_tool fzf "fzf --fish | source"
-    init_tool zoxide "zoxide init fish | source"
-    init_tool mise "mise activate fish | source"
-    init_tool navi "navi widget fish | source"
+    set -l cache_dir $HOME/.cache/shell_init
+    if not test -d $cache_dir
+        mkdir -p $cache_dir
+    end
+
+    function cache_fish_init
+        set -l cache_dir $HOME/.cache/shell_init
+        set -l tool $argv[1]
+        set -l init_cmd $argv[2]
+        if not test -f $cache_dir/$tool.fish
+            if type -q $tool
+                eval $init_cmd > $cache_dir/$tool.fish
+            end
+        end
+        if test -f $cache_dir/$tool.fish
+            source $cache_dir/$tool.fish
+        end
+    end
+
+    cache_fish_init fzf "fzf --fish"
+    cache_fish_init zoxide "zoxide init fish"
+    cache_fish_init mise "mise activate fish"
+    cache_fish_init navi "navi widget fish"
+
+    functions -e cache_fish_init
 
     # Source shell config files
     for file in aliases.sh local.sh
